a8fb31de178475c598c78a2e4694b379892100cc
IBEHOKUTO <hokuto.ibe@simple-way.jp>
2018-12-18 17:56:17 +0900

    update

--- ios/Classes/FirebaseAdmobPlatformViewPlugin.m
+++ ios/Classes/FirebaseAdmobPlatformViewPlugin.m
@@ -1,5 +1,5 @@
 #import "FirebaseAdmobPlatformViewPlugin.h"
-#import <firebase_admob_platform_view/firebase_admob_platform_view-Swift.h>
+// #import <firebase_admob_platform_view/firebase_admob_platform_view-Swift.h>
 
 @implementation FirebaseAdmobPlatformViewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
